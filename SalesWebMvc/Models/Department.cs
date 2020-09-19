6386b1986323b5c5498739de7ae0ec2979c6ce17
Luciano Melo <luciano_melopereira@hotmail.com>
2020-09-19 18:28:08 -0300

    Seeding Service

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -14,7 +14,7 @@ namespace SalesWebMvc.Models
         {
         }
 
-        public Department(int id, string name, int myProperty)
+        public Department(int id, string name)
         {
             Id = id;
             Name = name;
